a319ceaf063533b08ff458443efd273e097320d4
shilpa85 <s.dhapola85@gmail.com>
2021-10-05 12:54:32 +0530

    Add files via upload
    Custom port added 8181

--- mobile-app-ws/src/main/java/com/mobile/app/ws/ui/controller/UserController.java
+++ mobile-app-ws/src/main/java/com/mobile/app/ws/ui/controller/UserController.java
@@ -1,24 +1,55 @@
 package com.mobile.app.ws.ui.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("users") //http:localhost:8080/users
+import com.mobile.app.ws.ui.model.request.UserDetailsRequestModel;
+import com.mobile.app.ws.ui.model.response.UserRest;
+
+//to building RESTful web services, HTTP requests are handled by a controller with @RestController annotation
+@RestController  
+@RequestMapping("/users")   
 public class UserController {
 	
 	@GetMapping
-	public String getUser() {
-		return "test get.....";
+	public String getUser(@RequestParam(value="page", defaultValue="1") int page,  
+			@RequestParam(value="limit", defaultValue="50") int limit) {
+		return "test get page =" + page + "limit = " + limit;
 	}
 	
+	@GetMapping(path="/{userId}")
+	public ResponseEntity<UserRest> getUser(@PathVariable String userId) {
+		UserRest returnValue = new UserRest();
+		returnValue.setFirstName("shilpa");
+		returnValue.setLastName("singh");
+		returnValue.setEmail("shilpa@gmail.com");
+		returnValue.setUserId("1");
+		
+		return new ResponseEntity<UserRest>(returnValue,HttpStatus.OK) ;
+	}
+
+
 	@PostMapping
-	public String createUser() {
-		return "test create";
+	public ResponseEntity<UserRest> createUser(@Valid @RequestBody UserDetailsRequestModel userDetails) {
+		UserRest returnValue = new UserRest();
+		returnValue.setFirstName(userDetails.getFirstName());
+		returnValue.setLastName(userDetails.getLastName());
+		returnValue.setEmail(userDetails.getEmail());
+		//returnValue.setPassword(userDetails.getPassword());
+		
+		return new ResponseEntity<UserRest>(returnValue,HttpStatus.OK) ;
+		
 	}
 	
 	@PutMapping
@@ -30,5 +61,4 @@ public class UserController {
 	public String deleteUser() {
 		return "test delete";
 	}
-
 }
---
+++ mobile-app-ws/src/main/java/com/mobile/app/ws/ui/model/request/UserDetailsRequestModel.java
@@ -0,0 +1,50 @@
+package com.mobile.app.ws.ui.model.request;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class UserDetailsRequestModel {
+	@NotNull(message="Firstname can not be empty.")
+	private String firstName;
+	@NotNull(message="Lastname can not be empty.")
+	private String lastName;
+	@NotNull(message="Email can not be empty.")
+	@Email
+	private String email;
+	@NotNull(message="Password can not be empty.")
+	@Size(min=8, max=16, message="Password must be more than 8 characters and less than 16 characters.")
+	private String password;
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+}
